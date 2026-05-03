8c0a5e4c6ce6c51085f6f18ec377377fe620aa88
Guilherme0112 <guimendesmen124@gmail.com>
2026-05-03 13:53:38 -0300

    fix: add entrypoint para comandos extras

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 # Roda as migrations
 php artisan migrate --force
 
-# Inicia o servidor (ajuste para o seu comando de início, ex: apache, nginx ou artisan)
-php artisan serve --host=0.0.0.0 --port=$PORT
+# Roda os seeds
+php artisan db:seed --force
+
+# O 'exec "$@"' faz com que o container execute o CMD do Dockerfile
+# que no seu caso é o: /usr/bin/supervisord -c /etc/supervisord.conf
+exec "$@"
