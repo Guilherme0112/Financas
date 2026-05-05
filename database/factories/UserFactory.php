27a3478bfb47d3a94385173c3f45076b9708fa6a
Guilherme0112 <guimendesmen124@gmail.com>
2026-05-04 21:47:05 -0300

    fix: correções no user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,10 +28,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'phone' => fake()->phoneNumber(),
+            'phone' => $this->faker->phoneNumber(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
